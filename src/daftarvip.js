1e18722f32cb68d96df5a53eed7d95c8c7e6ebb1
Admcauss-alt <77009854+Admcauss-alt@users.noreply.github.com>
2021-02-27 20:25:07 -0300

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/554792091566 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO CAUSS 🐊🚩 :*
+_https://chat.whatsapp.com/IrKmqmFSUsUA1NUPS7D52l_ `
+}
+exports.daftarvip = daftarvip
